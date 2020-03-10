592ec0f4a84493d94adb8c5a85e0e372ced229de
cesar <csaralbrto@gmail.com>
2020-03-10 12:45:11 -0500

    first commit

---
+++ resources/views/infografia.blade.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PH8JSZ9');
+    </script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-37954209-3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-37954209-3');
+    </script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/slick/slick/slick.min.js"></script>
+    <script src="//tt.mbww.com/tt-ff805620597e92258a4fdf2324268ecb9704a8d0600924efc10ff253eddeab01.js" async></script>
+    <link rel="stylesheet" type="text/css" href="js/slick/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="js/slick/slick/slick-theme.css" />
+    <link href="https://fonts.googleapis.com/css?family=Passion+One:400,700,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/styles.css?v=5">
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.slider').slick({
+                arrows: false,
+                autoplay: true,
+            });
+        });
+    </script>
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PH8JSZ9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <div class="container-fluid">
+        <header>
+            <div class="container">
+                <div class="row logo">
+                    <div class="col-sm mt-5">
+                        <img src="img/logo-win-plus.png" width="195">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm text-center hero-razones">
+                        <img src="img/razones.png">
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="slider">
+            <div><img src="img/Comerciales_WIN_Sports_013.jpg"></div>
+            <div><img src="img/Comerciales_WIN_Sports_018.jpg"></div>
+            <div><img src="img/Comerciales_WIN_Sports_061.jpg"></div>
+        </div>
+        <div class="container">
+            <div class="row intro-infografia">
+                <div class="col-sm pt-5 pr-5 pb-5 pl-5 text-center">
+                    <h2 class="mt-4 mb-3 d-none d-md-block">¡PÍDELO YA!</h2>
+                    <h2 class="mt-4 mb-3 d-block d-md-none">¡LLAMA Y PÍDELO YA!</h2>
+                    <h2 class="mt-4 mb-3">Haz clic en el logo de tu operador</h2>
+                    <div class="operadores d-none d-md-block">
+                        <div class="operador">
+                            <a href="https://www.directv.com.co/comprar/canales-premium/winplus" target="_blank" gtmOutbound="Link" gtmProvider="Directv" gtmDevice="Desktop"><img src="img/operadores/directv.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="https://www.claro.com.co/personas/app/solicitudes/solicitud-producto-nv/?prod=television&promo=tvwin&canal_trafico=ORGANICO" target="_blank" gtmOutbound="Link" gtmProvider="Claro" gtmDevice="Desktop"><img src="img/operadores/claro.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="https://www.movistar.co/television" target="_blank" gtmOutbound="Link" gtmProvider="Movistar" gtmDevice="Desktop"><img src="img/operadores/movistar.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="https://www.tigo.com.co/servicios/television/winsports" target="_blank" gtmOutbound="Link" gtmProvider="Tigo" gtmDevice="Desktop"><img src="img/operadores/tigo.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="https://etb.com/hogares/adicionales-winsports.aspx" target="_blank" gtmOutbound="Link" gtmProvider="Etb" gtmDevice="Desktop"><img src="img/operadores/etb.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="https://www.emcali.com.co/web/guest" target="_blank" gtmOutbound="Link" gtmProvider="Emcali" gtmDevice="Desktop"><img src="img/operadores/emcali.jpg" ></a>
+                        </div>
+                    </div>
+
+                     <div class="operadores d-block d-md-none">
+                        <div class="operador">
+                            <a href="tel:0315185656" gtmOutbound="Link" gtmProvider="Directv" gtmDevice="Mobile"><img src="img/operadores/directv.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="tel:0317500500" gtmOutbound="Link" gtmProvider="Claro" gtmDevice="Mobile"><img src="img/operadores/claro.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="tel:018000911009" gtmOutbound="Link" gtmProvider="Movistar" gtmDevice="Mobile"><img src="img/operadores/movistar.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="tel:0344444141" gtmOutbound="Link" gtmProvider="Tigo" gtmDevice="Mobile"><img src="img/operadores/tigo.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="tel:0313777777" gtmOutbound="Link" gtmProvider="Etb" gtmDevice="Mobile"><img src="img/operadores/etb.jpg" ></a>
+                        </div>
+                        <div class="operador">
+                            <a href="tel:0325240177" gtmOutbound="Link" gtmProvider="Emcali" gtmDevice="Mobile"><img src="img/operadores/emcali.jpg" ></a>
+                        </div>
+                    </div>
+                    <h2 class="mt-5 mb-5">Esta infografía tiene<br> las respuestas que buscas</h2>
+                    <img src="img/infografia-winsportsmas.jpg" class="infografia">
+                </div>
+            </div>
+        </div>
+        <div class="container cta-section">
+            <div class="row">
+                <div class="col-sm text-center">
+                    <a href="#" class="cta font-source openBtn" data-toggle="modal" data-target="#modalInfografia">Ver video</a>
+                </div>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="modalInfografia" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    </div>
+                    <div class="modal-body">
+                        <iframe width="100%" height="480" src="//mdstrm.com/embed/5e00d0d1f6002f0881f86a68" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container preguntas mt-5 mb-4">
+            <div class="row">
+                <div class="col-sm-4 mb-4">
+                    <div class="card card-flip h-100">
+                        <div class="card-front text-white bg-gray">
+                            <div class="card-body justify-content-center align-self-center text-center">
+                                <img src="img/que-es.png">
+                                <h3 class="card-title">¿Qué es<br> Win Sports Más?</h3>
+                            </div>
+                        </div>
+                        <div class="card-back bg-blue">
+                            <div class="card-body justify-content-center align-self-center">
+                                <img src="img/que-es.png" width="40" class="mb-3">
+                                <p class="card-text font-source text-center">Es el nuevo canal de Win Sports, con 24 horas de programación. Ahí podrás ver el fútbol profesional colombiano en vivo, sin comerciales durante los partidos y con más cámaras. También tendrás 4  nuevos programas con mejores ayudas tecnológicas. Es un canal 100% dedicado al fútbol.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mb-4">
+                    <div class="card card-flip h-100">
+                        <div class="card-front text-white bg-gray">
+                            <div class="card-body justify-content-center align-self-center text-center">
+                                <img src="img/suscripcion.png">
+                                <h3 class="card-title">¿Cómo hago para tenerlo?</h3>
+                            </div>
+                        </div>
+                        <div class="card-back bg-blue">
+                            <div class="card-body justify-content-center align-self-center">
+                                <img src="img/suscripcion.png" width="40" class="mb-3">
+                                <p class="card-text font-source text-center">Tienes que suscribirte, contactando a tu operador de televisión.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mb-4">
+                    <div class="card card-flip h-100">
+                        <div class="card-front text-white bg-gray">
+                            <div class="card-body justify-content-center align-self-center text-center">
+                                <img src="img/canal.png">
+                                <h3 class="card-title">¿Qué operadores de televisión lo van a tener?</h3>
+                            </div>
+                        </div>
+                        <div class="card-back bg-blue">
+                            <div class="card-body justify-content-center align-self-center">
+                                <img src="img/canal.png" width="40" class="mb-3">
+                                <p class="card-text font-source text-center">Puedes contactar a cualquiera de estos operadores en tu ciudad.</p>
+                                <div class="text-center">
+                                    <img src="img/claro.png" width="40">
+                                    <img src="img/emcali.png" width="40">
+                                    <img src="img/direct-tv.png" width="40">
+                                    <img src="img/tigo.png" width="40">
+                                    <img src="img/movistar.png" width="40">
+                                    <!-- <img src="img/hv-tv.png" width="40"> -->
+                                    <img src="img/etb.png" width="40">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mb-4">
+                    <div class="card card-flip h-100">
+                        <div class="card-front text-white bg-gray">
+                            <div class="card-body justify-content-center align-self-center text-center">
+                                <img src="img/cuanto.png">
+                                <h3 class="card-title">¿Cuánto vale y desde cuándo lo puedo ver?</h3>
+                            </div>
+                        </div>
+                        <div class="card-back bg-blue">
+                            <div class="card-body justify-content-center align-self-center">
+                                <img src="img/cuanto.png" width="40" class="mb-3">
+                                <p class="card-text font-source text-center">Pídelo a tu operador de televisión por $29.900 mensuales y verás más de 60 partidos en promedio en vivo, ($500 pesos por partido). A partir del 20 de enero de 2020 lo podrás disfrutar.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<!--                 <div class="col-sm-3 mb-4">
+                    <div class="card card-flip h-100">
+                        <div class="card-front text-white bg-gray">
+                            <div class="card-body justify-content-center align-self-center text-center">
+                                <img src="img/cuando.png">
+                                <h3 class="card-title">¿Desde cuándo lo puedo pedir?</h3>
+                            </div>
+                        </div>
+                        <div class="card-back bg-blue">
+                            <div class="card-body justify-content-center align-self-center">
+                                <img src="img/cuando.png" width="40" class="mb-3">
+                                <p class="card-text font-source text-center">Si llamas ya a tu operador, recibirás una gran oferta.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-4">
+                    <div class="card card-flip h-100">
+                        <div class="card-front text-white bg-gray">
+                            <div class="card-body justify-content-center align-self-center text-center">
+                                <img src="img/cuando.png">
+                                <h3 class="card-title">¿Cuándo puedo ver Win Sports Más?</h3>
+                            </div>
+                        </div>
+                        <div class="card-back bg-blue">
+                            <div class="card-body justify-content-center align-self-center">
+                                <img src="img/cuando.png" width="40" class="mb-3">
+                                <p class="card-text font-source text-center">A partir del 20 de enero de 2020.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+                <div class="col-sm-4 mb-4">
+                    <div class="card card-flip h-100">
+                        <div class="card-front text-white bg-gray">
+                            <div class="card-body justify-content-center align-self-center text-center">
+                                <img src="img/partidos.png">
+                                <h3 class="card-title">¿Qué partidos puedo ver en <br>Win Sports Más?</h3>
+                            </div>
+                        </div>
+                        <div class="card-back bg-blue">
+                            <div class="card-body justify-content-center align-self-center">
+                                <img src="img/partidos.png" width="40" class="mb-3">
+                                <p class="card-text font-source text-center">En cada fecha verás todos los de la Liga, 3 de Torneo, 4 de Copa y 1 de Liga Femenina. También, los partidos de la Superliga colombiana. El campeonato difutbol y hasta 4 partidos de la liga turca.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mb-4">
+                    <div class="card card-flip h-100">
+                        <div class="card-front text-white bg-gray">
+                            <div class="card-body justify-content-center align-self-center text-center">
+                                <img src="img/canal.png">
+                                <h3 class="card-title">¿Puedo ver el fútbol profesional colombiano en otro canal?</h3>
+                            </div>
+                        </div>
+                        <div class="card-back bg-blue">
+                            <div class="card-body justify-content-center align-self-center">
+                                <img src="img/canal.png" width="40" class="mb-3">
+                                <p class="card-text font-source text-center">Todos los partidos de la liga Colombiana, los podras ver en Win Sports Más y solo algunos por el canal Win Sports de siempre.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="bg-blue-dark">
+            <div class="container pt-5 pb-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="img/logo-win-plus.png" width="195">
+                    </div>
+                    <div class="col-md-6 mt-3 font-source text-white text-right">
+                        <p>Copyright &copy; 2019, WINSPORTS Bogotá - Colombia. Todos los derechos reservados.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
